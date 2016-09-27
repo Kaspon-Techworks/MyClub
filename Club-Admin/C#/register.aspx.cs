486bbf7b672d117e8d83d705afb6f25d703ceece
Vetri98 <vetriselvam.r@kaspontech.com>
2016-09-27 10:52:24 +0530

    Added Sql Exception
    -Vetri

--- Club-Admin/C#/register.aspx.cs
+++ Club-Admin/C#/register.aspx.cs
@@ -30,15 +30,29 @@ namespace Club
 
         protected void Button1_Click(object sender, EventArgs e)
         {
-            conn = new SqlConnection("Data Source=USER-PC;Initial Catalog=club;User Id=sa;Password=vetri123");
-            string qry = "  insert into ClubRegister(clubName,adminName,adrs,city,stat,pin,phone,Email,typeofclub,stas,ClubTypeId) values('" + TextBox1.Text + "','" + TextBox2.Text + "','" + TextBox3.Text + "','" + TextBox4.Text + "','" + TextBox5.Text + "','" + TextBox6.Text + "','" + TextBox7.Text + "','" + TextBox8.Text + "','" + DropDownList1.SelectedItem + "'," + 0 + ","+DropDownList1.SelectedValue+")";
-            conn.Open();    
-            SqlCommand cmd = new SqlCommand(qry, conn);
-            int a = cmd.ExecuteNonQuery();
-            conn.Close();
-            if (a > 0) 
+            try
             {
-                Label10.Text = "Register Successfully";
+                conn = new SqlConnection("Data Source=USER-PC;Initial Catalog=club;User Id=sa;Password=vetri123");
+                string qry = "  insert into ClubRegister(clubName,adminName,adrs,city,stat,pin,phone,Email,typeofclub,stas,ClubTypeId) values('" + TextBox1.Text + "','" + TextBox2.Text + "','" + TextBox3.Text + "','" + TextBox4.Text + "','" + TextBox5.Text + "','" + TextBox6.Text + "','" + TextBox7.Text + "','" + TextBox8.Text + "','" + DropDownList1.SelectedItem + "'," + 0 + "," + DropDownList1.SelectedValue + ")";
+                conn.Open();
+                SqlCommand cmd = new SqlCommand(qry, conn);
+                int a = cmd.ExecuteNonQuery();
+                conn.Close();
+                if (a > 0)
+                {
+                    Label10.Text = "Register Successfully";
+                }
+            }
+            catch (SqlException sqlEx)
+            {
+                if (sqlEx.Message.StartsWith("Violation of PRIMARY KEY constraint 'PK__ClubRegi__266CC91F286302EC'. Cannot insert duplicate key in object 'dbo.ClubRegister'. "))
+                {
+                    TextBox1.Style.Add("border-color", "Red");
+                    Response.Write("<script>alert('Club Name Already register please try again with some other name..')</script>");
+                    
+                }
+                else
+                    throw;
             }
 
         }
